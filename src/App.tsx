6616e145a0351064863c38f5c2225fbc52c5c288
ashirbad <ashirbadbehera29@gmail.com>
2022-01-27 00:01:37 +0530

    🚀 add some things

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import Card from './components/Card';
 import ItemsBag from './components/ItemsBag';
@@ -14,6 +14,8 @@ type BagType = {
 function App() {
   const [machineState, setMachineState] = useState(INITIAL_VENDING_DATA);
   const [bagState, setBagState] = useState<Array<BagType>>([]);
+  const [inputMoney, setInputMoney] = useState(0);
+  const [change, setChange] = useState<{ note: number; no: number }[]>([]);
 
   const addToBag = (product: ItemType) => {
     const bagItem = bagState.find((item) => item.key === product.key);
@@ -68,6 +70,36 @@ function App() {
     );
   };
 
+  const calculateCost = () => {
+    return bagState.reduce((acc, item) => acc + item.total_price, 0);
+  };
+
+  const totalCost = useMemo(calculateCost, [bagState]);
+
+  const handleChange = (cost: number) => {
+    const arr = [500, 200, 100, 50, 20, 10, 5, 2, 1];
+    const ss = [];
+
+    let i = 0;
+    while (i < arr.length && cost) {
+      const noOfNote = Math.floor(cost / arr[i]);
+      cost = cost % arr[i];
+      if (noOfNote) ss.push({ note: arr[i], no: noOfNote });
+      i++;
+    }
+
+    setChange(ss);
+  };
+
+  const handleBuy = () => {
+    if (bagState.length === 0 || inputMoney === 0) return;
+
+    const totalCost = bagState.reduce((acc, item) => acc + item.total_price, 0);
+
+    if (totalCost > inputMoney) return;
+    handleChange(inputMoney - totalCost);
+  };
+
   return (
     <main className="min-h-screen max-w-2xl mx-auto">
       <header className="px-5 py-16 text-center">
@@ -86,7 +118,38 @@ function App() {
           ))}
         </div>
 
+        <div className="max-w-2xl mx-auto px-4 mt-7 text-center">
+          <span>Total Cost: {totalCost} &#8377;</span>
+        </div>
+
         <ItemsBag itemBag={bagState} />
+
+        <div className="mt-5  max-w-2xl mx-auto px-8 flex justify-center gap-4">
+          <input
+            type="number"
+            min={0}
+            value={inputMoney}
+            placeholder="anything...."
+            onChange={(e) => setInputMoney(Number(e.target.value))}
+            className="border px-3 rounded-md focus:outline-none focus:ring-2"
+          />
+          <button
+            className="bg-green-400 px-3 py-1 rounded-md text-white"
+            onClick={handleBuy}>
+            Buy 🌟
+          </button>
+        </div>
+
+        {change && change.length > 0 && (
+          <div className="max-w-2xl mx-auto px-8 mt-5 flex gap-6 flex-wrap">
+            {change.map((item) => (
+              <div key={item.note} className="flex gap-4 bg-orange-300 px-2 rounded-md">
+                <span>{item.note}</span>
+                <span>{item.no}</span>
+              </div>
+            ))}
+          </div>
+        )}
       </section>
     </main>
   );
--- src/components/ItemsBag.tsx
+++ src/components/ItemsBag.tsx
@@ -1,3 +1,5 @@
+import { XIcon } from '@heroicons/react/solid';
+
 type BagType = {
   key: string;
   quantity: number;
@@ -9,10 +11,12 @@ const ItemsBag = ({ itemBag }: { itemBag: Array<BagType> }) => {
   return (
     <div className="max-w-2xl mx-auto mt-8 px-8 border rounded-md py-4">
       {itemBag && itemBag.length > 0 ? (
-        <div>
+        <div className="flex gap-2">
           {itemBag.map((item) => (
-            <div key={item.key}>
-              {item.label} X {item.quantity}
+            <div
+              key={item.key}
+              className="border bg-green-300 px-2 rounded-md shadow-sm flex items-center gap-1 text-gray-700">
+              {item.label} <XIcon className="h-4 w-4" /> {item.quantity}
             </div>
           ))}
         </div>
